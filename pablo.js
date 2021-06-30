d6dab609c3b0342e25fd1f1913d7ee18c98f9750
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2021-06-30 17:13:05 -0400

    Add: New hydra patch.

--- pablo.js
+++ pablo.js
@@ -11,12 +11,12 @@ window.onload = function () {
   // see options to change parameters
   // osc().out()
   
-  shape(200,0.5,1.5)
+  shape(2,0.5,1.5)
   .scale(() => a.fft[0]*1.25 + 0.05)
   .color([0.5,2].smooth(1),0.3,0)
-  .repeat(2,2)
-  .modulateScale(osc(3,0.5),-0.6)
-  .add(o0,0.5)
+  .repeat(4,4)
+  .modulateScale(osc(0.2,0.5),-0.6)
+  .add(o0,0.8)
   .scale(0.9) 
   .out()
 
